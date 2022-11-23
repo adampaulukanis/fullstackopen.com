b29e1a5e845cf603477705dbc28933f21bd76c65
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-11-23 12:13:33 +0000

    part1d: unicafe step4 display statistics only when feedback has been gathered
    + add missing statistics: all, positive and average.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,5 @@
 import { useState } from 'react'
 
-const sanitize = (value) => {
-    if (isNaN(value)) {
-        return 0
-    }
-    return value
-}
-
 const Statistics = (props) => {
     let value = props.value
     if (props.name.toLowerCase() === 'positive') {
@@ -24,31 +17,70 @@ const Statistics = (props) => {
     )
 }
 
+const History = (props) => {
+    if (props.allStats.length !== 0) {
+        return (
+            <>
+                <Statistics name="good" value={props.good}/>
+                <Statistics name="neutral" value={props.neutral}/>
+                <Statistics name="bad" value={props.bad}/>
+                <Statistics name="all" value={props.good + props.neutral + props.bad}/>
+                <Statistics name="average" value={(props.good * 1 + props.neutral * 0 + props.bad * -1) / (props.good + props.neutral + props.bad)}/>
+                <Statistics name="positive" value={props.good / (props.good + props.neutral + props.bad)}/>
+            </>
+        )
+    }
+
+    return <>No feedback given.</>
+}
+
 const App = () => {
     const [ good, setGood ] = useState(0)
     const [ neutral, setNeutral ] = useState(0)
     const [ bad, setBad ] = useState(0)
+    const [ allStats, setStats ] = useState([]);
+
+    const handleGood = () => {
+        setStats(allStats.concat('G'))
+        setGood(good + 1);
+    }
+
+    const handleNeutral = () => {
+        setStats(allStats.concat('N'))
+        setNeutral(neutral + 1)
+    }
+
+    const handleBad = () => {
+        setStats(allStats.concat('B'))
+        setBad(bad + 1)
+    }
 
     return (
         <>
             <h1>Give feedback</h1>
             <div className="grading-area">
-                <button onClick={() => setGood(good + 1)}>good</button>
-                <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-                <button onClick={() => setBad(bad + 1)}>bad</button>
+                <button onClick={handleGood}>good</button>
+                <button onClick={handleNeutral}>neutral</button>
+                <button onClick={handleBad}>bad</button>
             </div>
 
             <div className="statistics">
                 <h2>Statistics</h2>
-                <Statistics name="good" value={good}/>
-                <Statistics name="neutral" value={neutral}/>
-                <Statistics name="bad" value={bad}/>
-                <Statistics name="all" value={good + neutral + bad}/>
-                <Statistics name="average" value={sanitize((good - bad) / (good + neutral + bad))}/>
-                <Statistics name="positive" value={sanitize(good / (good + neutral + bad))}/>
+
+                <History allStats={allStats} good={good} neutral={neutral} bad={bad}/>
             </div>
         </>
     )
 }
 
 export default App
+
+/*
+ *
+                <Statistics name="good" value={good}/>
+                <Statistics name="neutral" value={neutral}/>
+                <Statistics name="bad" value={bad}/>
+                <Statistics name="all" value={good + neutral + bad}/>
+                <Statistics name="average" value={sanitize((good - bad) / (good + neutral + bad))}/>
+                <Statistics name="positive" value={sanitize(good / (good + neutral + bad))}/>
+                */
