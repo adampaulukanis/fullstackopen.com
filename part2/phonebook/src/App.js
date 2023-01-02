d1f53bbb72ca883e46dd0432506877b45a6fbb9d
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-02 17:14:09 +0000

    part2b: exercise 2.8: phonebook-step3: done
    Expand your application by allowing users to add phone numbers to the
    phone book.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,31 @@ import './App.css'
 import { useState } from 'react'
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-    ])
+    const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
-        if (newName === '') {
-            console.log('Trying to add empty string')
+        if (newName === '' || newNumber === '') {
+            alert('Trying to add empty string')
             return
         }
         // Check if the name already is added
         if (persons.some(p => p.name === newName)) {
             alert(`${newName} is already in the phonebook!`)
             setNewName('')
+            setNewNumber('')
             return
         }
         // It's OK, so add a new name
         const nameObject = {
             name: newName,
+            number: newNumber,
         }
         setPersons(persons.concat(nameObject))
         setNewName('')
+        setNewNumber('')
     }
 
     const handleNameChange = (event) => {
@@ -32,16 +35,33 @@ const App = () => {
         }
         setNewName(event.target.value)
     }
+    const handleNumberChange = (event) => {
+        const number = event.target.value
+        if (number === '') {
+            return
+        }
+        setNewNumber(number)
+    }
 
     return (
         <>
             <h2>Phonebook</h2>
             <form onSubmit={addName}>
                 <div>
-                    name: <input 
-                                value={newName}
-                                onChange={handleNameChange}
-                            />
+                        <label htmlFor="name">name:</label>
+                        <input
+                            name="name"
+                            value={newName}
+                            onChange={handleNameChange}
+                        />
+                </div>
+                <div>
+                    <label htmlFor="number">number:</label>
+                    <input
+                        name="number"
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                    />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -50,7 +70,7 @@ const App = () => {
             <h2>Numbers</h2>
             <ul id="persons">
                 {persons.map(p =>
-                    <li key={p.name}>{p.name}</li>
+                    <li key={p.name}>{p.name} : {p.number}</li>
                 )}
             </ul>
         </>
