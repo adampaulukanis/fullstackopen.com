d91ad8e5f435bffe9cb97fa27c39ce8da99e6203
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-02 20:19:47 +0000

    part2b: exercise 2.9: phonebook-step4: done
    Implement a search field that can be used to filter the list of people
    by name.
    
    + Add some styling. Labels need a right margin between them and input
      elements.
    + Add trim() on input values.
    + Doing this exercise I found out that there was a bug in my code. I was
      unable to remove the very first character typed into the input
      element. The solution to that was to use
      setNew{Name,Number,whatever}('') in the proper places.
      I think [this](https://stackoverflow.com/questions/62952756/unable-to-remove-first-character-from-search-in-input-react-js) helped me a bit.
      It was not clear, but when I saw the question and answers I had an
      idea how to fix my problem. Thank you!

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,11 +5,14 @@ const App = () => {
     const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+    const [ showOnly, setShowOnly ] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
         if (newName === '' || newNumber === '') {
             alert('Trying to add empty string')
+            setNewName('')
+            setNewNumber('')
             return
         }
         // Check if the name already is added
@@ -21,16 +24,19 @@ const App = () => {
         }
         // It's OK, so add a new name
         const nameObject = {
-            name: newName,
-            number: newNumber,
+            name: newName.trim(),
+            number: newNumber.trim(),
         }
         setPersons(persons.concat(nameObject))
         setNewName('')
         setNewNumber('')
+
+        document.querySelector('#name').focus()
     }
 
     const handleNameChange = (event) => {
         if (event.target.value === '') {
+            setNewName('')
             return
         }
         setNewName(event.target.value)
@@ -38,18 +44,36 @@ const App = () => {
     const handleNumberChange = (event) => {
         const number = event.target.value
         if (number === '') {
+            setNewNumber('')
             return
         }
         setNewNumber(number)
     }
+    const handleFilterChange = (event) => {
+        const findThisName = event.target.value
+        setShowOnly(findThisName)
+    }
+
+    const personsToShow = showOnly
+        ? persons.filter(p => p.name.toLowerCase().includes(showOnly.toLowerCase()))
+        : persons
 
     return (
         <>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+
+            <label htmlFor="filter">filter shown with:</label>
+            <input
+                name="filter"
+                onChange={handleFilterChange}
+            />
+
+            <h2>Add a new</h2>
             <form onSubmit={addName}>
                 <div>
                         <label htmlFor="name">name:</label>
                         <input
+                            id="name"
                             name="name"
                             value={newName}
                             onChange={handleNameChange}
@@ -58,6 +82,7 @@ const App = () => {
                 <div>
                     <label htmlFor="number">number:</label>
                     <input
+                        id="number"
                         name="number"
                         value={newNumber}
                         onChange={handleNumberChange}
@@ -67,9 +92,10 @@ const App = () => {
                     <button type="submit">add</button>
                 </div>
             </form>
+
             <h2>Numbers</h2>
             <ul id="persons">
-                {persons.map(p =>
+                {personsToShow.map(p =>
                     <li key={p.name}>{p.name} : {p.number}</li>
                 )}
             </ul>
