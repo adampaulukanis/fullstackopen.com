7a282145dd602b06c89cd8e7274041357f031747
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-07 16:37:43 +0000

    react-state-and-lifecycle: convert Clock into a class
    The render method will be called each time an update happens, but as
    long as we render `<Clock />` into the same DOM node, only a single
    instance of the Clock class will be used.

--- part2/react-state-and-lifecycle/src/components/Clock.js
+++ part2/react-state-and-lifecycle/src/components/Clock.js
@@ -1,10 +1,14 @@
-function Clock(props) {
-    return (
-        <div className="clock">
-            <h1>Hello, World!</h1>
-            <h2>It is {props.date.toLocaleString()}</h2>
-        </div>
-    )
+import React from 'react'
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div className="clock">
+                <h1>Hello, World!</h1>
+                <h2>It is {this.props.date.toLocaleString()}</h2>
+            </div>
+        )
+    }
 }
 
 export default Clock
