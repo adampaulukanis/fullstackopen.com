64e1329bc6ba62c8dfd205a062408bce3cecf3e0
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-12 02:11:50 +0000

    react-forms: controlled component: select element

--- part2/react-form/src/index.js
+++ part2/react-form/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import NameForm from './components/NameForm'
-import EssayForm from './components/EssayForm'
+import FlavorForm from './components/FlavorForm'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <NameForm />
-        <br />
-        <EssayForm />
+        <FlavorForm />
     </React.StrictMode>
 );
