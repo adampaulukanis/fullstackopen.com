e74fda386b9636d9c582c7bfd641f4a12ee12f17
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-12 00:31:50 +0000

    react-forms: introduction
    Simple HTML form accepts a single name.

---
+++ part2/react-form/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="App">
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+}
+
+export default App;
