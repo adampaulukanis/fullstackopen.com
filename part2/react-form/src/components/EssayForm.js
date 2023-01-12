486c36f195ed8fa01d025a0dcd9ce40061dbb6ea
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-01-12 01:22:35 +0000

    react-forms: controlled component: textarea element

---
+++ part2/react-form/src/components/EssayForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class EssayForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 'Please write an essay about what you like the most.'
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value)
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Essay:
+                    <textarea
+                        value={this.state.value}
+                        onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+export default EssayForm
--- part2/react-form/src/index.js
+++ part2/react-form/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import NameForm from './components/NameForm'
+import EssayForm from './components/EssayForm'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <NameForm />
+        <br />
+        <EssayForm />
     </React.StrictMode>
 );
