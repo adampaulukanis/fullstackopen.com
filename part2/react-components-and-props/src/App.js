716170ee8901edfb8fd824e4111b2eb3cb3d0ae3
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-02-28 12:31:23 +0000

    Remove react tutorials - they should not be here

--- part2/react-components-and-props/src/App.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-function App() {
-  return (
-      <div>Hello</div>
-  )
-}
-
-export default App
--- part2/react-components-and-props/src/components/Welcome.js
+++
@@ -1,5 +0,0 @@
-function Welcome(props) {
-    return <h1>Hello, '{props.name}'</h1>
-}
-
-export default Welcome
