5eeee0debefe4962b51f3f713ebbcc26e4818e5a
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-03-04 23:05:37 +0000

    part2c: exercise notes: use axios and json-server with frontend

--- exercises/notes/src/index.js
+++ exercises/notes/src/index.js
@@ -1,28 +1,14 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import axios from "axios"
 
-import App from './App'
+axios
+    .get("http://localhost:3001/notes")
+    .then((response) => {
+        const notes = response.data
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2022-12-21T00:30:37.097Z',
-        important: false,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2022-12-21T00:32:37.123Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are methods of HTTP protocol',
-        date: '2022-12-21T00:33:37.342Z',
-        important: true,
-    },
-]
+        ReactDOM.createRoot(document.getElementById("root")).render(
+            <App notes={notes}/>
+        )
+    })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes}/>
-)
