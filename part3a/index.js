db600dba7c8907a2dab0efb7747eca9c590c8b4e
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-05-01 01:55:27 +0100

    PART3A: Simple Web Server

--- part3a/index.js
+++ part3a/index.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+import http from "http"
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "application/json" })
+    response.end(JSON.stringify(notes))
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log("Server running on port ", PORT)
+})
