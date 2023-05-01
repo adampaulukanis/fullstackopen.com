d7811a50dea45d0cd365e53685529b33e53ad900
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-05-01 02:14:06 +0100

    PART3A: Add Express and nodemon

--- part3a/index.js
+++ part3a/index.js
@@ -1,4 +1,5 @@
-import http from "http"
+import express from "express"
+const app = express()
 
 let notes = [
     {
@@ -18,9 +19,12 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "application/json" })
-    response.end(JSON.stringify(notes))
+app.get("/", (request, response) => {
+    response.send("<h1>Hello, world!</h1>")
+})
+
+app.get("/api/notes", (request, response) => {
+    response.json(notes)
 })
 
 const PORT = 3001
