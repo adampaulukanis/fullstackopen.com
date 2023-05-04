dc4c613ae5b88f3435318c0763a41ec1ba31ec4e
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-05-04 01:56:30 +0100

    PART3A: Receiving Data
    Ready to move on to the exercises.

--- part3a/README.md
+++ part3a/README.md
@@ -6,3 +6,4 @@ In the course *Postman* is used. I do not want to use it, so I decided to go wit
 
 Ex.:
 `curl -i -X DELETE http://localhost:3001/api/notes/3`
+`curl -d '{"name": "Adam"}' -H 'Content-Type: application/json' http://localhost:3001/api/notes`
--- part3a/index.js
+++ part3a/index.js
@@ -19,6 +19,8 @@ let notes = [
     }
 ]
 
+app.use(express.json())
+
 app.all("*", (request, response, next) => {
     console.log({
         method: request.method,
@@ -53,6 +55,32 @@ app.delete("/api/notes/:id", (request, response) => {
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
 })
 
 const PORT = 3001
