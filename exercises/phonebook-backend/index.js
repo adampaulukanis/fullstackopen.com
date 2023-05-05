add0394e887ecec9071f39c774d72e4db3a5a15e
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-05-05 11:24:44 +0100

    part3a ex phonebook backend step1 done
    Implement a Node app that returns a hard coded list of phone book entries from
    the address http://localhost:3001/api/persons.

---
+++ exercises/phonebook-backend/index.js
@@ -0,0 +1,34 @@
+const persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+import express from "express"
+const app = express()
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log("Server running on port ", PORT)
+})
